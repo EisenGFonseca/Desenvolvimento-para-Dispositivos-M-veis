83ab99a235a9bbf6c47d5c4c185d8022023c911e
Eisen Fonseca <eisen.fonseca@gmail.com>
2021-08-31 03:49:07 -0400

    Aula 1.7.5 AppController e AppWidget
    Aula 1.7.5 AppController e AppWidget

--- Curso na pratica/ola_mundo/lib/app_widget.dart
+++ Curso na pratica/ola_mundo/lib/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'app_controller.dart';
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -9,12 +10,14 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-        brightness: Brightness.dark
-      ),
-      home: HomePage(),
-    );
+    return AnimatedBuilder(
+        animation: AppController.instance,
+        builder: (BuildContext context, child) {
+          return MaterialApp(
+            theme: ThemeData(
+                primaryColor: Colors.blue, brightness: Brightness.dark),
+            home: HomePage(),
+          );
+        });
   }
 }
