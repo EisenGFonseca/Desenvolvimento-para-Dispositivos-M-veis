0d6a7b07d5d40c22953bc2dee54c26babd4b21d6
Eisen Fonseca <eisen.fonseca@gmail.com>
2021-08-31 03:35:56 -0400

    Aula 1.6.3 Align
    Aula 1.6.3 Align

--- Curso na pratica/ola_mundo/lib/home_page.dart
+++ Curso na pratica/ola_mundo/lib/home_page.dart
@@ -2,44 +2,43 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  State<HomePage> createState(){
+  State<HomePage> createState() {
     return HomePageState();
   }
 }
 
 class HomePageState extends State<HomePage> {
-
-int counter = 0;
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-
     var container = Container(
         height: 150,
         width: 150,
         color: Colors.red,
-        child: Container(
-           height: 50,
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            height: 50,
             width: 50,
             color: Colors.green,
-        )
-    );
+            child: Icon(Icons.remove_circle_outline),
+        )));
 
     return Scaffold(
-    appBar: AppBar(
-      title: Text(
+      appBar: AppBar(
+          title: Text(
         "App Flutter ADS",
-        )
-      ),
+      )),
       body: container,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add_circle),
-          onPressed: () {
-            setState(() {
-                counter ++;
-              });
-          },
-          ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_circle),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
     );
   }
 }
