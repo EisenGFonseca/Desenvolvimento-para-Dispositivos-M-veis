eba8ef7cbe024fcd2bd81a88041bbaf868b4a796
Eisen Fonseca <eisen.fonseca@gmail.com>
2021-08-30 20:43:44 -0400

    Curso Cod3er
    Andamento do curso cod3r.
    1.3

---
+++ Curso Cod3r/flutter_crud/lib/models/user.dart
@@ -0,0 +1,14 @@
+
+class User {
+  final id;
+  final name;
+  final email;
+  final avatarUrl;
+
+  const User({
+    this.id,
+    required this.name,
+    required this.email,
+    required this.avatarUrl,
+  });
+}
